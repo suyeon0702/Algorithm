b3c0f35a239bcf9cfcc6e32e5de1ec6f8310d9cf
suyeon0702 <42757784+suyeon0702@users.noreply.github.com>
2025-01-18 14:41:10 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -2,7 +2,19 @@ num_list = []
 for i in range(9):
     n = int(input())
     num_list.append(n)
-max_num = max(num_list)
-max_num_nth = num_list.index(max_num)+1
+
+# max, index로 편하게 가기
+# max_num = max(num_list)
+# max_num_nth = num_list.index(max_num)+1
+# print(max_num)
+# print(max_num_nth)
+
+max_num = -1
+max_num_nth = 0
+for i in range(9):
+    num = num_list[i]
+    if num > max_num:
+        max_num = num
+        max_num_nth = i+1
 print(max_num)
 print(max_num_nth)
